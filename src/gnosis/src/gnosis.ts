5741c0731bfb544967a3a17c480ca3287b2886d1
yathishram <123yathish.r@gmail.com>
2022-07-22 17:40:33 +0400

    initial commit

---
+++ src/gnosis/src/gnosis.ts
@@ -0,0 +1,177 @@
+import { AddressZero } from '@ethersproject/constants'
+import { Web3Provider } from '@ethersproject/providers'
+import Safe from '@gnosis.pm/safe-core-sdk'
+import { SafeSignature } from '@gnosis.pm/safe-core-sdk-types'
+import EthersAdapter from '@gnosis.pm/safe-ethers-lib'
+import axios from 'axios'
+import { ethers } from 'ethers'
+import { SafeTransactionTemplate } from '../utils/types'
+
+export class Gnosis {
+  private safeAddress = ''
+  private provider: Web3Provider | null = null
+  private chainId = 0
+  private etherAdapter: EthersAdapter | null = null
+  private EIP712_SAFE_TX_TYPE = {
+    SafeTx: [
+      { type: 'address', name: 'to' },
+      { type: 'uint256', name: 'value' },
+      { type: 'bytes', name: 'data' },
+      { type: 'uint8', name: 'operation' },
+      { type: 'uint256', name: 'safeTxGas' },
+      { type: 'uint256', name: 'baseGas' },
+      { type: 'uint256', name: 'gasPrice' },
+      { type: 'address', name: 'gasToken' },
+      { type: 'address', name: 'refundReceiver' },
+      { type: 'uint256', name: 'nonce' },
+    ],
+  }
+
+  /**
+   *
+   * @param provider Web3Provider - The provider to use for interacting with the Gnosis Safe
+   * @param chainId number - The chain ID of the Wallet
+   */
+  constructor(provider: Web3Provider, chainId: number) {
+    this.provider = provider
+    this.chainId = chainId
+  }
+
+  private buildSafeTx = (template: SafeTransactionTemplate) => {
+    return {
+      to: template.to,
+      value: template.value || 0,
+      data: template.data || '0x',
+      operation: template.operation || 0,
+      safeTxGas: template.safeTxGas || 0,
+      baseGas: template.baseGas || 0,
+      gasPrice: template.gasPrice || 0,
+      gasToken: template.gasToken || AddressZero,
+      refundReceiver: template.refundReceiver || AddressZero,
+      nonce: template.nonce,
+    }
+  }
+
+  /**
+   *
+   * @param baseTx SafeTransactionTemplate - The base transaction template to use for the signature
+   */
+  private estimateSafeTx = async (baseTx: any) => {
+    try {
+      const result = await axios.post(
+        `https://safe-relay.rinkeby.gnosis.io/api/v2/safes/${this.safeAddress}/transactions/estimate/`,
+        baseTx
+      )
+      return result.data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  private proposeTx = async (
+    safeTx: any,
+    safeTxHash: string,
+    signature: SafeSignature
+  ) => {
+    try {
+      const data = {
+        ...safeTx,
+        contractTransactionHash: safeTxHash,
+        sender: signature.signer,
+        signature: signature.data,
+      }
+      const res = await axios.post(
+        `https://safe-transaction.rinkeby.gnosis.io/api/v1/safes/${this.safeAddress}/multisig-transactions/`,
+        data
+      )
+      return res.data
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  /**
+   *
+   * @param chainId number - The chain ID of the Wallet
+   * @param safeTx any - The SafeTransactionTemplate to use for the signature
+   * @returns string - The transaction hash of the proposed transaction
+   */
+  private getTransactionHash = (chainId: number, safeTx: any): string => {
+    const safeTxHash = ethers.utils._TypedDataEncoder.hash(
+      {
+        chainId: chainId,
+        verifyingContract: this.safeAddress,
+      },
+      this.EIP712_SAFE_TX_TYPE,
+      safeTx
+    )
+    return safeTxHash
+  }
+
+  gnosisInit = async (
+    safeAddress: string,
+    receiverAddress: string,
+    paymentValueInWei: string
+  ): Promise<any> => {
+    try {
+      this, (safeAddress = safeAddress)
+
+      if (this.provider) {
+        const safeOwner = await this.provider.getSigner(0)
+
+        //Create Ethers Adapter
+
+        this.etherAdapter = new EthersAdapter({
+          ethers,
+          signer: safeOwner,
+        })
+
+        const safeSdk: Safe = await Safe.create({
+          ethAdapter: this.etherAdapter,
+          safeAddress: this.safeAddress,
+        })
+
+        const safeTx = this.buildSafeTx({
+          to: ethers.utils.getAddress(receiverAddress),
+          value: paymentValueInWei,
+          data: '0x',
+          operation: 0,
+          safeTxGas: 0,
+          nonce: 0,
+        })
+
+        //Step 1: Estimate SafeTx
+
+        const estimatedSafeTx = await this.estimateSafeTx(safeTx)
+        console.log(estimatedSafeTx)
+
+        safeTx.safeTxGas = estimatedSafeTx.safeTxGas
+        safeTx.nonce =
+          estimatedSafeTx.lastUsedNonce === null
+            ? 0
+            : estimatedSafeTx.lastUsedNonce + 1
+
+        //Step 2: get safetx hash
+        const safeTxHash = this.getTransactionHash(this.chainId, safeTx)
+        console.log(safeTxHash)
+
+        //Step 3: sign safeTxHash
+        const signature = await safeSdk.signTransactionHash(safeTxHash)
+
+        //Step 4: propose safeTx
+        const proposedSafeTx = await this.proposeTx(
+          safeTx,
+          safeTxHash,
+          signature
+        )
+
+        console.log(proposedSafeTx)
+        return proposedSafeTx
+      } else {
+        return null
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
