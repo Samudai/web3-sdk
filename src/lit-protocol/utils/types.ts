7b1f76039edbaf382295dfe08494effbb8b52bb7
yathishram <123yathish.r@gmail.com>
2022-07-25 16:21:43 +0400

    fixes minor issues

---
+++ src/lit-protocol/utils/types.ts
@@ -0,0 +1,19 @@
+export type AccessControlConditions = {
+  contractAddress: string
+  standardContractType: string
+  chain: string
+  method: string
+  parameters: string[]
+  returnValueTest: {
+    comparator: string
+    value: string
+  }
+}
+
+export type RecourceId = {
+  baseUrl: string
+  path: string
+  orgId: string
+  role: string
+  extraData: string
+}
