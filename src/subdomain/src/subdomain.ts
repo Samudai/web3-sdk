e497942fec42aefe8b6a7d2e150c3402ea6607cd
yathishram <123yathish.r@gmail.com>
2022-11-05 16:09:19 +0400

    updates subdomain for verification

--- src/subdomain/src/subdomain.ts
+++ src/subdomain/src/subdomain.ts
@@ -2,22 +2,32 @@ import { ethers } from 'ethers'
 import { TransactionResponse, Web3Provider } from '@ethersproject/providers'
 import ContractABI from '../contracts/abi.json'
 import { ErrorResponse } from '../utils/types'
+import axios from 'axios'
+import { GatewayURL } from '../utils/constants'
 
 export class Subdomain {
   private chainId: number | null = null
   private contractAddress = ''
-
-  constructor() {
+  private gatewayURL = ''
+  constructor(env: string) {
     //this.chainId = chainId
+    this.gatewayURL = GatewayURL.find((item) => item.env === env)?.url || ''
     this.contractAddress = '0x125ff9e0F79371C67512A002d7890aF2aD9CeA09'
   }
 
   claimSubdomain = async (
     username: string,
-    provider: Web3Provider
+    provider: Web3Provider,
+    member_id: string
   ): Promise<boolean | ErrorResponse> => {
     try {
-      if (provider && this.contractAddress) {
+      const memberCountResult = await axios.get(
+        `${this.gatewayURL}/get/invitecount/${member_id}`
+      )
+
+      const memberCount = memberCountResult.data.data.constructor
+
+      if (provider && this.contractAddress && memberCount >= 5) {
         const signer = provider.getSigner()
         const address = await signer.getAddress()
         const contract = new ethers.Contract(
