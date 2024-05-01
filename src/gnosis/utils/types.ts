655fc5be17d7c0ad623a845b7ffee46c9f81cb44
Venky <venkatesh@samudai.xyz>
2024-05-01 09:04:30 +0530

    alchemy create webhhook

---
+++ src/gnosis/utils/types.ts
@@ -0,0 +1,213 @@
+import { Web3Provider } from '@ethersproject/providers'
+import { SafeMultisigTransactionResponse } from '@safe-global/safe-core-sdk-types'
+export {
+  MetaTransactionData,
+  SafeSignature,
+  SafeTransactionData,
+  SafeTransactionDataPartial,
+} from '@gnosis.pm/safe-core-sdk-types'
+
+export {
+  SafeBalanceUsdResponse,
+  SafeInfoResponse,
+  SafeMultisigTransactionListResponse,
+  SafeMultisigTransactionResponse,
+  SignatureResponse,
+} from '@gnosis.pm/safe-service-client'
+
+export enum ProviderType {
+  'WALLET' = 'WALLET',
+  'GNOSIS' = 'GNOSIS',
+}
+
+export type GnosisTx = {
+  safeAddress: string
+  provider: Web3Provider
+  chainId: number
+  receiver: string
+  valueInWei: string
+}
+
+export type SafeTransactionTemplate = {
+  to: string
+  value?: string
+  data?: string
+  operation?: number
+  safeTxGas?: number
+  baseGas?: number
+  gasPrice?: number
+  gasToken?: string
+  refundReceiver?: string
+  nonce: number
+}
+
+export type ErrorResponse = {
+  message: string
+  error: string
+  safeTxHash?: string
+}
+
+export type SafeTransactionResponse = {
+  safeTxHash: string
+  proposedSafeTx: any
+}
+
+export type SafeTransactions = {
+  count: number
+  next: string | null
+  previous: string | null
+  results: MultisigTransactionResponse[]
+}
+
+export type MultisigTransactionResponse = {
+  safe: string
+  to: string
+  value: string
+  data: string | null
+  operation: number
+  gasToken: string
+  safeTxGas: string
+  baseGas: string
+  gasPrice: string
+  refundReceiver: string
+  nonce: string
+  executionDate: string
+  submissionDate: string
+  modified: string
+  blockNumber: number
+  transactionHash: string
+  safeTxHash: string
+  executor: string
+  isExecuted: boolean
+  isSuccessful: boolean
+  ethGasPrice: string
+  maxFeePerGas: string
+  maxPriorityFeePerGas: string
+  gasUsed: string
+  fee: string
+  origin: string
+  dataDecoded: string
+  confirmationsRequired: number
+  confirmations: MultisigConfirmations[]
+  trusted: boolean
+  signatures: string
+}
+
+export type SafeBalanceUsdResponsePortal = {
+  key: string
+  name: string
+  decimals: number
+  symbol: string
+  price: number
+  address: string
+  network: string
+  updatedAt: string
+  createdAt: string
+  liquidity: number
+  image: string
+  tokenId: string
+  balanceUSD: number
+  balance: number
+  rawBalance: string
+}
+
+export type MultisigConfirmations = {
+  description: string
+  owner: string
+  submissionDate: string
+  transactionHash: string
+  signature: string
+  signatureType: string
+}
+
+export type Provider = {
+  id: string
+  dao_id: string
+  provider_type: ProviderType
+  address: string
+  chain_id: string
+  created_by: string
+  created_at: string
+  updated_at: string
+}
+
+export type UserSafe = {
+  safeAddress: string
+  owners: string[] | null
+}
+
+export type SafeExecutionStatus = {
+  isOwner: boolean
+  isConfirmation?: boolean
+  isExecutable?: boolean
+}
+
+export type CustomERC20Token = {
+  tokenAddress: string | null
+  value: string
+}
+
+export type TransactionDetails = {
+  safeMultisigTransactionResponse: SafeMultisigTransactionResponse
+  confirmation: number
+}
+
+export enum TransactionNature {
+  'BATCH' = 'BATCH',
+  'SINGLE' = 'SINGLE',
+  'OTHER' = 'OTHER',
+}
+export enum TransactionType {
+  'SENT' = 'SENT',
+  'REJECTION' = 'REJECTION',
+  'RECEIVED' = 'RECEIVED',
+  'OTHER' = 'OTHER',
+}
+export type TxDetails = {
+  walletAddress: string
+  currency: string
+  amount: number
+  logo: string
+  amountUSD: number
+}
+
+export type TxObject = {
+  nonce: number
+  type: TransactionType //batch
+  nature: TransactionNature
+  date: string
+  amountUSD: number
+  transactionDetails: TxDetails[]
+  confirmationsRequired: number
+  confirmations: string[] // array of owners
+  safeTxHash: string
+}
+
+export type TxHistoryDetails = {
+  from: string
+  walletAddress: string
+  currency: string
+  amount: number
+  logo: string
+  amountUSD: number
+}
+
+export type TxHistoryObject = {
+  nonce: number
+  type: TransactionType //batch
+  nature: TransactionNature
+  executionDate: string
+  amountUSD: number
+  transactionDetails: TxHistoryDetails[]
+  executedBy: string
+  safeTxHash: string
+  txHash: string
+  url: string
+}
+
+export type WidgetBalance = {
+  symbol: string
+  balance: number
+  usdValue: number
+  tokenAddress: string
+}
